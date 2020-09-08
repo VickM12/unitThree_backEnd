f309880c49ff75610ffa8618355cbb77503f35c8
Vicktor M <vicktor.moberg@gmail.com>
2020-09-08 12:36:03 -0400

    added middleware to allow access-controll-allow-origin

--- Spoofy-Back-End/server.js
+++ Spoofy-Back-End/server.js
@@ -24,6 +24,10 @@ db.on('open', () => {
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 // app.use(express.static('public'))
 
 
